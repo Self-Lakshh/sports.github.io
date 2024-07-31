e3d84a2f16a889ab8be9eaf69c16c2cf6248c0d3
Self-nasu <124767257+Self-nasu@users.noreply.github.com>
2024-07-31 13:27:18 +0530

    Role verification lock Done

---
+++ javascript/adminrole.js
@@ -0,0 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBRMjjr5remFfzHiScxfmXK79JwWQ7c-3M",
+  authDomain: "login-lakshh.firebaseapp.com",
+  projectId: "login-lakshh",
+  storageBucket: "login-lakshh.appspot.com",
+  messagingSenderId: "214721614258",
+  appId: "1:214721614258:web:c3bebff238d815d25994ab",
+  measurementId: "G-4EEXFMT14S",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, async (user) => {
+  if (user) {
+    const userRef = doc(db, "userRoles", user.uid);
+    const userDoc = await getDoc(userRef);
+
+    if (!userDoc.exists()) {
+      window.location.href = "login.html";
+    } else {
+      const role = userDoc.data().role;
+      if (role !== "admin") {
+        window.location.href = "login.html";
+      }
+      // User is authorized, you can load your admin.html content here
+    }
+  } else {
+    window.location.href = "login.html";
+  }
+});
